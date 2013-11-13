e1a166ad083cce29b1564e479ddd8358af6098b4
Mark Richman <mark@markrichman.com>
2013-11-13 16:44:32 -0500

    Updates

--- amazonmws/mwsclient.py
+++ amazonmws/mwsclient.py
@@ -1,36 +1,6 @@
 # -*- coding: utf-8 -*-
 
-""" Amazon Order Report Fields:
-['order-id', 'order-item-id', 'purchase-date', 'payments-date', 'buyer-email',
-'buyer-name', 'buyer-phone-number', 'sku', 'product-name',
-'quantity-purchased', 'currency', 'item-price', 'item-tax', 'shipping-price',
-'shipping-tax', 'ship-service-level', 'recipient-name', 'ship-address-1',
-'ship-address-2', 'ship-address-3', 'ship-city', 'ship-state',
-'ship-postal-code', 'ship-country', 'ship-phone-number', 'tax-location-code',
-'tax-location-city', 'tax-location-county', 'tax-location-state',
-'per-unit-item-taxable-district', 'per-unit-item-taxable-city',
-'per-unit-item-taxable-county', 'per-unit-item-taxable-state',
-'per-unit-item-non-taxable-district', 'per-unit-item-non-taxable-city',
-'per-unit-item-non-taxable-county', 'per-unit-item-non-taxable-state',
-'per-unit-item-zero-rated-district', 'per-unit-item-zero-rated-city',
-'per-unit-item-zero-rated-county', 'per-unit-item-zero-rated-state',
-'per-unit-item-tax-collected-district', 'per-unit-item-tax-collected-city',
-'per-unit-item-tax-collected-county', 'per-unit-item-tax-collected-state',
-'per-unit-shipping-taxable-district', 'per-unit-shipping-taxable-city',
-'per-unit-shipping-taxable-county', 'per-unit-shipping-taxable-state',
-'per-unit-shipping-non-taxable-district', 'per-unit-shipping-non-taxable-city',
-'per-unit-shipping-non-taxable-county', 'per-unit-shipping-non-taxable-state',
-'per-unit-shipping-zero-rated-district', 'per-unit-shipping-zero-rated-city',
-'per-unit-shipping-zero-rated-county', 'per-unit-shipping-zero-rated-state',
-'per-unit-shipping-tax-collected-district',
-'per-unit-shipping-tax-collected-city',
-'per-unit-shipping-tax-collected-county',
-'per-unit-shipping-tax-collected-state', 'item-promotion-discount',
-'item-promotion-id', 'ship-promotion-discount', 'ship-promotion-id',
-'delivery-start-date', 'delivery-end-date', 'delivery-time-zone',
-'delivery-Instructions', 'sales-channel']
-
-Sample Order Report:
+""" Sample Order Report:
 order-id	order-item-id	purchase-date	payments-date	buyer-email	buyer-name	buyer-phone-number	sku	product-name	quantity-purchased	currency	item-price	item-tax	shipping-price	shipping-tax	ship-service-level	recipient-name	ship-address-1	ship-address-2	ship-address-3	ship-city	ship-state	ship-postal-code	ship-country	ship-phone-number	tax-location-code	tax-location-city	tax-location-county	tax-location-state	per-unit-item-taxable-district	per-unit-item-taxable-city	per-unit-item-taxable-county	per-unit-item-taxable-state	per-unit-item-non-taxable-district	per-unit-item-non-taxable-city	per-unit-item-non-taxable-county	per-unit-item-non-taxable-state	per-unit-item-zero-rated-district	per-unit-item-zero-rated-city	per-unit-item-zero-rated-county	per-unit-item-zero-rated-state	per-unit-item-tax-collected-district	per-unit-item-tax-collected-city	per-unit-item-tax-collected-county	per-unit-item-tax-collected-state	per-unit-shipping-taxable-district	per-unit-shipping-taxable-city	per-unit-shipping-taxable-county	per-unit-shipping-taxable-state	per-unit-shipping-non-taxable-district	per-unit-shipping-non-taxable-city	per-unit-shipping-non-taxable-county	per-unit-shipping-non-taxable-state	per-unit-shipping-zero-rated-district	per-unit-shipping-zero-rated-city	per-unit-shipping-zero-rated-county	per-unit-shipping-zero-rated-state	per-unit-shipping-tax-collected-district	 per-unit-shipping-tax-collected-city	per-unit-shipping-tax-collected-county	per-unit-shipping-tax-collected-state	item-promotion-discount	item-promotion-id	ship-promotion-discount	ship-promotion-id	delivery-start-date	delivery-end-date	delivery-time-zone	delivery-Instructions	sales-channel
 113-0132185-7763468	49867438371106	2013-11-06T10:05:33-08:00	2013-11-06T10:05:33-08:00	10zlwn9nypqx7p7@marketplace.amazon.com	john salas	301-659-0945	11904	Advanced-Q Ubiquinol with VESIsorb�.	2	USD	69.90	0.00	3.00	0.00	Standard	john salas	1402 LINWOOD DR			SALINAS	CALIFORNIA	93906-3348	US	301-659-0945	050532950	SALINAS	MONTEREY	CA	0.00	0.00	0.00	0.00	69.90	69.90	69.90	69.90	0.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00	3.00	3.00	3.00	3.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00		0.00
 002-0854791-5713047	20069603398042	2013-11-06T16:08:44-08:00	2013-11-06T16:08:44-08:00	1fb2xks5g88jpyz@marketplace.amazon.com	michele  j derby	9514610658	10400	Nutri-Health Flora Source Multi-Probiotic Capsules	1	USD	31.89	0.00	3.00	0.00	Standard	michele  j derby	39055 LOS GATOS DR			MURRIETA	CA	92563-7871	US	9514610658	050650000	NOT APPLICABLE	RIVERSIDE	CA	0.00	0.00	0.00	0.00	31.89	0.00	31.89	31.89	0.00	31.89	0.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00	3.00	0.00	3.00	3.00	0.00	3.00	0.00	0.00	0.00	0.00	0.00	0.00	0.00		0.00
@@ -44,9 +14,81 @@ import logging
 from csv import reader
 from StringIO import StringIO
 
-from boto import mws, set_stream_logger
+from boto import set_stream_logger
 from boto.mws import connection
 from boto.mws.exception import ResponseError
+
+ORDER_REPORT_FIELDS = [
+    'order-id',
+    'order-item-id',
+    'purchase-date',
+    'payments-date',
+    'buyer-email',
+    'buyer-name',
+    'buyer-phone-number',
+    'sku',
+    'product-name',
+    'quantity-purchased',
+    'currency',
+    'item-price',
+    'item-tax',
+    'shipping-price',
+    'shipping-tax',
+    'ship-service-level',
+    'recipient-name',
+    'ship-address-1',
+    'ship-address-2',
+    'ship-address-3',
+    'ship-city',
+    'ship-state',
+    'ship-postal-code',
+    'ship-country',
+    'ship-phone-number',
+    'tax-location-code',
+    'tax-location-city',
+    'tax-location-county',
+    'tax-location-state',
+    'per-unit-item-taxable-district',
+    'per-unit-item-taxable-city',
+    'per-unit-item-taxable-county',
+    'per-unit-item-taxable-state',
+    'per-unit-item-non-taxable-district',
+    'per-unit-item-non-taxable-city',
+    'per-unit-item-non-taxable-county',
+    'per-unit-item-non-taxable-state',
+    'per-unit-item-zero-rated-district',
+    'per-unit-item-zero-rated-city',
+    'per-unit-item-zero-rated-county',
+    'per-unit-item-zero-rated-state',
+    'per-unit-item-tax-collected-district',
+    'per-unit-item-tax-collected-city',
+    'per-unit-item-tax-collected-county',
+    'per-unit-item-tax-collected-state',
+    'per-unit-shipping-taxable-district',
+    'per-unit-shipping-taxable-city',
+    'per-unit-shipping-taxable-county',
+    'per-unit-shipping-taxable-state',
+    'per-unit-shipping-non-taxable-district',
+    'per-unit-shipping-non-taxable-city',
+    'per-unit-shipping-non-taxable-county',
+    'per-unit-shipping-non-taxable-state',
+    'per-unit-shipping-zero-rated-district',
+    'per-unit-shipping-zero-rated-city',
+    'per-unit-shipping-zero-rated-county',
+    'per-unit-shipping-zero-rated-state',
+    'per-unit-shipping-tax-collected-district',
+    'per-unit-shipping-tax-collected-city',
+    'per-unit-shipping-tax-collected-county',
+    'per-unit-shipping-tax-collected-state',
+    'item-promotion-discount',
+    'item-promotion-id',
+    'ship-promotion-discount',
+    'ship-promotion-id',
+    'delivery-start-date',
+    'delivery-end-date',
+    'delivery-time-zone',
+    'delivery-Instructions',
+    'sales-channel']
 
 
 class MWS:
@@ -75,9 +117,12 @@ class MWS:
         """MWS GetReport/2009-01-01 API call; quota=15 restore=60.00
         Returns the contents of a report."""
         report = self.mws.get_report(ReportId=report_id)
-        fields = {}
+        data = []
         f = StringIO(report)
+        f.readline()  # skip header row
         for row in reader(f, delimiter='\t'):
-            fields = dict((field, '') for field in row)
-            break
-        return fields
+            #fields = dict((field, '') for field in row)
+            #break
+            zipped = zip(ORDER_REPORT_FIELDS, row)
+            data.append(zipped)
+        return data
